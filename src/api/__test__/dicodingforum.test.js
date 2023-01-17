3be1fd59645eadb2fa80e85e72526d3e2695779d
arkaanni <arkaannuruliman@gmail.com>
2023-01-17 20:40:05 +0700

    add failed test case

--- src/api/__test__/dicodingforum.test.js
+++ src/api/__test__/dicodingforum.test.js
@@ -1,2 +1,12 @@
+import { userApi } from '../dicodingforum';
+
 describe('test dicodingforum api', () => {
+  jest.spyOn(global, 'fetch');
+  it('login should return success', async () => {
+    fetch.mockImplementationOnce(() => Promise.resolve({
+      json: () => Promise.resolve({ status: 'fail' }),
+    }));
+    const resp = await userApi.login({ email: 'test@mail.com', password: 'passwordtest'});
+    expect(resp.success).toBeTruthy();
+  });
 });
