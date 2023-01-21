d157bde31f93a2c1228c456553675a3f4f5d1c2a
arkaanni <arkaannuruliman@gmail.com>
2023-01-21 19:44:38 +0700

    add Header component test

---
+++ src/component/__test__/Header.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Header from '../Header';
+
+describe('Header component test', () => {
+
+  it('should show header with login link when user is null', () => {
+    const { getByRole, queryByRole } = render(
+      <BrowserRouter>
+        <Header handleLogout={null} />
+      </BrowserRouter>
+    );
+    const loginLink = getByRole('link', { name: 'login' });
+    expect(loginLink).toBeInTheDocument();
+    expect(queryByRole('link', { name: 'logout' })).not.toBeInTheDocument()
+  });
+  
+  it('should show header with logout button when user is not null', async () => {
+    const onLogout = jest.fn();
+    const mockUser = {
+      id: 'user-1',
+      name: 'user 1',
+      email: 'test@test.com',
+    };
+    const { getByRole, queryByRole } = render(
+      <BrowserRouter>
+        <Header user={mockUser} onLogout={onLogout} />
+      </BrowserRouter>
+    );
+    expect(queryByRole('link', { name: 'login' })).not.toBeInTheDocument();
+    const logoutBtn = getByRole('button', { name: 'logout' });
+    expect(logoutBtn).toBeEnabled();
+    await userEvent.click(logoutBtn);
+    expect(onLogout).toHaveBeenCalledTimes(1);
+  });
+});
