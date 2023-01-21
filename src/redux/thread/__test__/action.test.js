98fa150e332e617770538d506d721930e889144c
arkaanni <arkaannuruliman@gmail.com>
2023-01-21 07:02:22 +0700

    update thread thunk test

--- src/redux/thread/__test__/action.test.js
+++ src/redux/thread/__test__/action.test.js
@@ -1,4 +1,4 @@
-import { Action, upvoteThread } from '../action';
+import { Action, addThread, createThread, setTagList, upvoteThread } from '../action';
 import { displayMessage, MessageType } from '../../message/action';
 import { hideLoading, showLoading } from 'react-redux-loading-bar';
 import { threadApi } from '../../../api/dicodingforum';
@@ -22,7 +22,7 @@ describe('upvoteThread thunk test', () => {
     expect(dispatch).toHaveBeenCalledWith(displayMessage());
     expect(displayMessage).toHaveBeenCalledWith({
       type: MessageType.ERROR,
-      text: 'login untuk memberikan vote',
+      text: 'login untuk memberikan upvote',
     });
   });
 
@@ -46,8 +46,76 @@ describe('upvoteThread thunk test', () => {
     expect(dispatch).toHaveBeenCalledWith(displayMessage());
     expect(displayMessage).toHaveBeenCalledWith({
       type: MessageType.INFO,
-      text: 'berhasil memberikan vote',
+      text: 'berhasil memberikan upvote',
     });
+    expect(dispatch).toHaveBeenCalledTimes(4);
+  });
+
+  it('should dispatch UNDO_UPVOTE action when unUpvote is true', async () => {
+    const dispatch = jest.fn();
+    threadApi.votes.neutralize.mockReturnValue(Promise.resolve({ success: true }));
+    const params = {
+      threadId: 'thread-1',
+      userId: 123,
+      unUpvote: true,
+    };
+    await upvoteThread(params)(dispatch);
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith({
+      type: Action.UNDO_UPVOTE,
+      payload: {
+        userId: params.userId
+      },
+    });
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(dispatch).toHaveBeenCalledWith(displayMessage());
+    expect(displayMessage).toHaveBeenCalledWith({
+      type: MessageType.INFO,
+      text: 'berhasil membatalkan upvote',
+    });
+    expect(dispatch).toHaveBeenCalledTimes(4);
+  });
+
+  it('should dispatch ADD_THREAD and SET_TAG_LIST action when create thread success', async () => {
+    const dispatch = jest.fn();
+    const getState = jest.fn(() => ({
+      threadList: [],
+    }));
+    const mockThread = {
+      id: 'thread-1',
+      title: 'Thread Pertama',
+      body: 'Ini adalah thread pertama',
+      category: 'General',
+      createdAt: '2021-06-21T07:00:00.000Z',
+      owner: {
+        id: 'users-1',
+        name: 'John Doe',
+        avatar: 'https://generated-image-url.jpg',
+      },
+      upVotesBy: [],
+      downVotesBy: [],
+      comments: [],
+    };
+    threadApi.create.mockReturnValue(Promise.resolve({
+      success: true,
+      data: { thread: mockThread },
+      message: 'berhasil membuat thread',
+    }));
+    await createThread({
+      title: mockThread.title,
+      category: mockThread.category,
+      body: mockThread.body,
+    })(dispatch, getState);
+    expect(dispatch).toHaveBeenCalledWith(showLoading());
+    expect(dispatch).toHaveBeenCalledWith(addThread(mockThread));
+    expect(dispatch).toHaveBeenCalledWith(setTagList({'General': 1}));
+    expect(dispatch).toHaveBeenCalledWith(hideLoading());
+    expect(dispatch).toHaveBeenCalledWith(displayMessage());
+    expect(displayMessage).toHaveBeenCalledWith({
+      type: MessageType.INFO,
+      text: 'berhasil membuat thread',
+    });
+    expect(dispatch).toHaveBeenCalledTimes(5);
   });
 
   afterEach(() => {
